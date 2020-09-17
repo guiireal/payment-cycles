162c48f9e0b572991d73d93c4820893692cc252d
guiireal <guii_real@hotmail.com>
2020-09-17 01:12:46 -0300

    Add custom errors

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório!";
+
 module.exports = mongoose.connect(
   `mongodb://${process.env.DB_HOST}/${process.env.DB_DATABASE}`,
   {
